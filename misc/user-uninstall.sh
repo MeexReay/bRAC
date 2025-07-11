e737ebd1ce7d89e038e47faa602c9d3c210299ba
MeexReay <meexreay@gmail.com>
2025-07-11 21:06:18 +0300

    chore: fix user uninstallation

--- misc/user-uninstall.sh
+++ misc/user-uninstall.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rm -rf ~/.config/bRAC ~/.local/share/bRAC ~/.local/share/applications/ru.themixray.bRAC.desktop
+rm -rf ~/.local/bin/bRAC ~/.config/bRAC ~/.local/share/bRAC ~/.local/share/applications/ru.themixray.bRAC.desktop
